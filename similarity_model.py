aba5e00953393165652028672215fb86db1c9692
Jedrek00 <jedrek.koscielniak@gmail.com>
2023-11-15 19:45:31 +0100

    Change function name

--- similarity_model.py
+++ similarity_model.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 
 class SimilarityModel(ABC):
     @abstractmethod
-    def calculate_similiarity(self) -> None:
+    def calculate_similarity(self) -> None:
         pass
 
     @abstractmethod
--- tfidf_model.py
+++ tfidf_model.py
@@ -1,6 +1,6 @@
 import numpy as np
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 from similarity_model import SimilarityModel
 
@@ -11,7 +11,7 @@ class TfiDfModel(SimilarityModel):
         self.sim_matrix = np.array([])
         self.tfidf = TfidfVectorizer(stop_words='english')
 
-    def calculate_similiarity(self):
+    def calculate_similarity(self):
         tfidf_matrix = self.tfidf.fit_transform(self.docs)
         self.sim_matrix = linear_kernel(tfidf_matrix, tfidf_matrix)
 
@@ -19,5 +19,5 @@ class TfiDfModel(SimilarityModel):
         sim_scores = list(enumerate(self.sim_matrix[idx]))
         sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
         sim_scores = sim_scores[1:11]
-        movie_indices = [i[0] for i in sim_scores]
-        return movie_indices
+        indices = [i[0] for i in sim_scores]
+        return indices
