fbdeef9f5afb11af38ffd587acdc1ff20ff6f238
PatrykHub <72169376+PatrykHub@users.noreply.github.com>
2024-01-25 16:47:29 +0100

    fixing_bugs

--- scripts/collaborative_model.py
+++ scripts/collaborative_model.py
@@ -1,3 +1,6 @@
+import sys  
+sys.path.insert(1, '../')
+
 import numpy as np
 import pandas as pd
 from typing import Dict, Union
@@ -5,24 +8,28 @@ from scipy import spatial
 from scripts.similarity_model import SimilarityModel
 
 
-class CollaboartiveModel(SimilarityModel):
+class CollaborativeModel(SimilarityModel):
     def __init__(self, docs: pd.DataFrame):
         super().__init__()
         self.docs = docs
-        self.reviews = pd.DataFrame(None, index=0, columns=docs.columns)
+        self.reviews = pd.DataFrame(None, index=[0], columns=docs.columns)
 
-    def calculate_similarity(self, idx: Union[int, list[int]]) -> list[int]:
-        similiarities = np.zeros_like(self.docs.shape[0])
+    def calculate_similarity(self) -> int:
+        similiarities = np.zeros(self.docs.shape[0])
         for x in self.docs.index:
-            similiarities[x] = spatial.distance.cosine(self.docs[x], self.reviews[0])
+            similiarities[x] = spatial.distance.cosine(self.docs.loc[x].tolist(), self.reviews.loc[0].tolist())
         return np.argmin(similiarities)
 
-    def get_recommendations(self, idx: Union[Dict[int][int], pd.Series]) -> list[int]:
+    def get_recommendations(self, idx: Union[Dict[str, int], pd.Series]) -> list[int]:
+        if type(idx) == dict:
+            idx = pd.Series(idx)
         for key, val in zip(idx.keys(), idx.values):
-            self.reviews[key] = val
+            self.reviews.loc[0, str(key)] = val
         self.reviews = self.reviews.sub(self.reviews.mean(axis=1), axis=0).fillna(0)
         closest_user = self.calculate_similarity()
         closest_reviews = self.docs.iloc[closest_user].squeeze()
-        closest_reviews = closest_reviews.drop(labels=idx.keys())
+        for key in idx.keys():
+            if str(key) in closest_reviews.keys():
+                closest_reviews = closest_reviews.drop(labels = str(key))
         closest_reviews = closest_reviews.sort_values(ascending=False)
-        return closest_reviews.keys()[:10]
+        return [int(key) for key in closest_reviews.keys()[:10]]
